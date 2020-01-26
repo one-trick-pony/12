9de9630372c60600cf9648e7f5183e89bb180595
one-trick-pony <60320544+one-trick-pony@users.noreply.github.com>
2020-01-26 17:41:20 +0100

    Add files via upload

---
+++ 11_Hausaufgabe_1.hs
@@ -0,0 +1,17 @@
+import Text.Show.Functions 
+
+--a)-----------------------------------------
+data BinTree a b = Blatt b | Knoten a (BinTree a b) deriving Show
+
+--b)------------------------------------------
+example :: BinTree (Int ->Bool) Char
+example = Knoten \x ->(x > 4)
+             ((Knoten \x ->(x*x == x) ((Blatt 'g') (Knoten \x->(x==0)((Blatt 'u') (Blatt 'l')))))
+              (Knoten \x ->(x >=7) ((Blatt 'f' ) (Blatt 'i')) 
+             ))
+
+--c)----------------------------------------------
+countInnerNodes:: BinTree a b -> Int
+countInnerNodes (Blatt b) = 0
+countInnerNodes (Knoten n a m) = 1 + countInnerNodes n + countInnerNodes m
+			 
